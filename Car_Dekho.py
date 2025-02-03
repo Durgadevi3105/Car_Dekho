622466235557e681e30388d52da48cf1e06b955a
Durgadevi Naveenkumar <durga.n3105@gmail.com>
2025-02-03 10:31:37 +0530

    Update Car_Dekho.py

--- Car_Dekho.py
+++ Car_Dekho.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # Load the trained model
 with open("/mount/src/car_dekho/model.pkl", 'rb') as file:
-    model = pickle.load(file
+    model = pickle.load(file)
 
 # Streamlit app
 st.title("Car Resale Value Predictor")
