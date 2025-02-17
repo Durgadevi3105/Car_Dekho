c12c5f24b80db951db43e8cfab97da7b31ba36e6
Durgadevi Naveenkumar <durga.n3105@gmail.com>
2025-02-18 02:08:03 +0530

    Update Car_Dekho.py

--- Car_Dekho.py
+++ Car_Dekho.py
@@ -3,6 +3,7 @@ import pandas as pd
 import base64
 import numpy as np
 import pickle
+from sklearn.linear_model import LinearRegression
 
 def set_image_local(image_path):
     with open(image_path, "rb") as file:
